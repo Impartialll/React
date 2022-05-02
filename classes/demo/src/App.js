0cedfa83dbecf3b963cea99ad1b2c7991c988c20
Impartialll <maxutkom@gmail.com>
2022-05-03 00:47:53 +0300

    first_step

---
+++ classes/demo/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Text from "./components/Text";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello,World</h1>
+      <Text />
+    </div>
+  );
+}
+
+export default App;
---
+++ classes/demo/src/components/Text.js
@@ -0,0 +1,6 @@
+import "./Text.css";
+
+const Text = () => {
+  return <h1 className="text"></h1>;
+};
+export default Text;
